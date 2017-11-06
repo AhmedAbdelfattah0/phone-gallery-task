50f49ed350bba431bb57cc3c78e8d26abe105c22
AhmedAbdelfattah0 <33193362+AhmedAbdelfattah0@users.noreply.github.com>
2017-11-06 20:48:54 +0200

    add type filter , change the phones.json file location and add the min and max price filter input

--- phons-gallary/phons-gallary/views/js/app.js
+++ phons-gallary/phons-gallary/views/js/app.js
@@ -1,1 +1,74 @@
-var myApp = angular.module('myApp', []);
+"user strict";
+var app = angular.module('myApp', []);
+ 
+ 
+app.controller("phoneGallaryCtrl", ['$http',"$filter", phoneGallaryCtrl]);
+function phoneGallaryCtrl($http, $filter) {
+    var vm = this;
+    vm.colorArry = [];
+    vm.prices = "1";
+    debugger
+    // to get the data from json file and bind it to the view
+    $http.get('data/phones.json').then(function (response) {
+        debugger
+        vm.phonseData = response.data;
+        // map new property in every length on the phone list to convert price from USD to EGP
+        for (var i = 0; i < vm.phonseData.length; i++) {
+            vm.phonseData[i]['priceEGP'] = (vm.phonseData[i].priceUSD * 17);
+            vm.colorArry.push(vm.phonseData[i].color);
+        }
+        console.log('vm.phonseData',vm.phonseData);
+    })
+
+    //to switch the price view between USD and EGP
+    vm.convertPrices = function (data) {
+        
+        if (data==1) {
+            vm.showEGP = true;
+            vm.showUSD = false;
+        } else if (data == 2){
+            vm.showUSD = true;
+            vm.showEGP = false;
+        }
+    }
+
+
+        //to filter the data with type
+
+    vm.searchBytype = function (pram) {
+        if (pram==1) {
+            $http.get('data/phones.json').then(function (response) {
+                
+                vm.phonseData = $filter('filter')(response.data, { type: 'Tablet' });
+        // map new property in every length on the phone list to convert price from USD to EGP
+
+                for (var i = 0; i < response.data.length; i++) {
+                    response.data[i]['priceEGP'] = (response.data[i].priceUSD * 17);
+
+                     
+
+                }
+                  
+                                
+            })
+        } else if(pram==2) {
+            $http.get('data/phones.json').then(function (response) {
+                
+                vm.phonseData = $filter('filter')(response.data, { type: 'Phone' });
+        // map new property in every length on the phone list to convert price from USD to EGP
+
+                for (var i = 0; i < response.data.length; i++) {
+                    response.data[i]['priceEGP'] = (response.data[i].priceUSD * 17);
+
+                    
+                }
+
+
+            })
+        }
+
+
+    }
+    vm.convertPrices(1) 
+
+}
